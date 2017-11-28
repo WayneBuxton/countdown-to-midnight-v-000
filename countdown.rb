a7b68791921fa69155cc69ba32df7dc126fd0267
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-11-28 15:41:44 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,10 +10,5 @@ def countdown(number)
 end
 
 def countdown_with_sleep(num_secs)
-  runtime = Benchmark.measure { countdown_with_sleep(5) }
-  if runtime.real.to_i >= 5
-    yield
-  else
-    false
-  end
+  
 end
